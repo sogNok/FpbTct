05b3ce907fce0f5691d0aa21af3f0e28c0e17930
unknown <cndtjq97@gmail.com>
2021-07-26 18:14:44 +0900

    add beginning of EoP_07

---
+++ Python Basic/EoP_chap07.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 17:12:56 2021
+
+@author: 이충섭
+"""
+
+# Chapter 7 organization
+
+A = [11, 22, 33, 44 ,55, 33, 22]
+
+print(A.index(33,5,7))
+print(A[5:7].index(33))
