6862cd09be193473899bd9d732d015b318ea42b4
csLEE <39114516+sogNok@users.noreply.github.com>
2021-12-07 19:47:00 +0900

    add day4 daily coding test

---
+++ Coding Test/BAEKJOON/2742_기찍 N.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 18:59:16 2021
+
+@author: 이충섭
+"""
+
+# 2742th, 기찍 N | class 1
+
+N = int(input())
+
+for n in range(N, 0, -1):
+    print(n)
---
+++ Coding Test/BAEKJOON/2884_알람 시계.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  7 19:10:56 2021
+
+@author: 이충섭
+"""
+
+# 2884th, 알람 시계 | class 1
+
+H, M = input().split()
+
+if int(M) >= 45:
+    print(H, int(M) - 45)
+else:
+    if int(H) != 0:
+        print(int(H) - 1, int(M) + 15)
+    else:
+        print(23, int(M) + 15)
+        
