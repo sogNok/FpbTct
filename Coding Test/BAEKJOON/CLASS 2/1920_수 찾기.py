3ac519b9e19b248e660db3e652dce634dcbbf8de
sognok <cndtjq97@gmail.com>
2021-12-21 00:24:04 +0900

    change day10 daily coding test

--- Coding Test/BAEKJOON/CLASS 2/1920_수 찾기.py
+++ Coding Test/BAEKJOON/CLASS 2/1920_수 찾기.py
@@ -6,81 +6,10 @@ Created on Mon Dec 20 16:30:41 2021
 """
 
 # 1920th, 수 찾기 | class 2
-A = [i for i in range(10)]
 
-print(A[0:10])
-
-
-#%%
-import sys # int(sys.stdin.readline().r l strip()) sys.stdout.write()
-def bi_find(x, sorted_list):
-    len_list = len(sorted_list)
-    
-    if len_list == 1:
-        if x == len_list:
-            return 1
-        else:
-            return 0
-    
-    middle_point = len_list // 2
-    predict_x = sorted_list[middle_point]
-    
-    if predict_x == x:
-        return 1
-    elif predict_x < x:
-        return bi_find(x, sorted_list[middle_point :])
-    else:
-        return bi_find(x, sorted_list[: middle_point])
-    
-input()
-
-A = sorted(map(int, input().split()))
-
-input()
-
-M = list(map(int, input().split()))
-
-for m in M:
-    print(bi_find(m, A))
-    
-#%%
-
-import sys # int(sys.stdin.readline().r l strip()) sys.stdout.write()
-
-def bi_find(x, sorted_list):
-    len_list = len(sorted_list)
-    
-    if len_list == 1:
-        if x == sorted_list[0]:
-            return 1
-        else:
-            return 0
-    
-    middle_point = len_list // 2
-    predict_x = sorted_list[middle_point]
-    
-    if predict_x <= x:
-        return bi_find(x, sorted_list[middle_point :])
-    else:
-        return bi_find(x, sorted_list[: middle_point])
-    
+import sys
 
 sys.stdin.readline()
-
-A = sorted(map(int, sys.stdin.readline().split()))
-
-sys.stdin.readline()
-
-M = list(map(int, sys.stdin.readline().split()))
-
-for m in M:
-    sys.stdout.write(str(bi_find(m, A))+'\n')
-    
-#%%
-
-import sys, bisect
-
-N = int(sys.stdin.readline())
 
 A = sorted(map(int, sys.stdin.readline().split()))
 
@@ -89,4 +18,36 @@ sys.stdin.readline()
 M = list(map(int, sys.stdin.readline().split()))
 
 for x in M:
-    sys.stdout.write('1\n' if bisect.bisect_left(A, x) < N and bisect.bisect_right(A, x) > 0 else '0\n')
+    l = 0
+    r = len(A) - 1
+    m = 0
+    
+    while r > l:
+        m = (r+l) // 2
+        
+        if x > A[m]:
+            l = m + 1
+        elif x < A[m]:
+            r = m - 1
+        else:
+            l = m
+            break
+
+    sys.stdout.write('1\n' if x == A[l] else '0\n')
+    
+#%%
+# 참조 
+ip = input
+def main():
+    ip()
+    n = ip().strip().split(' ')
+    ip()
+    
+    s = set(n)
+    r = ''
+    for k in input().strip().split(' '):
+        r += '1\n' if k in s else '0\n'
+    print(r)
+
+if __name__ == "__main__":
+    main()
