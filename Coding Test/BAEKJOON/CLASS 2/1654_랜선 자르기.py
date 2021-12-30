2b37db70aaf4b1a81ee7d09a4ab485f840e1ff0d
sognok <cndtjq97@gmail.com>
2021-12-30 21:40:27 +0900

    add day18 coding test

--- Coding Test/BAEKJOON/CLASS 2/1654_랜선 자르기.py
+++ Coding Test/BAEKJOON/CLASS 2/1654_랜선 자르기.py
@@ -12,30 +12,54 @@ K, N = map(int, sys.stdin.readline().split())
 
 Lan_cables = [int(sys.stdin.readline()) for _ in range(K)]
 
-s_length = sum(Lan_cables) // N
-length = s_length
-gap = s_length // 2
-
-while gap:
-    count = 0
-    for cable in Lan_cables:
-        count += cable // length
+top = sum(Lan_cables) // N
+bottom = 1
+#print('top', top)
+while top > bottom+1:
+    length = (top + bottom) // 2
     
-    if count > N:
-        length -= gap
-    elif count < N:
-        length += gap
-    else:
+    count1 = 0
+    count2 = 0
+    for cable in Lan_cables:
+        count1 += cable // length
+        count2 += cable // (length+1)
+
+    #print(length, bottom, top, count1, count2)
+    if count1 > N:
+        bottom = length
+    elif count2 == 0 and count1 == N:
+        top = length
         break
-    
-    gap //= 2
+    elif count2 < N-1:
+        top = length
+    elif count1 == N-1:
+        top = length
+    elif count2 == N:
+        bottom = length
+    else:
+        top = length
+        break
 
-for leng in range(length, s_length+1):
-    count = 0
-    for cable in Lan_cables:
-        count += cable // leng
-    if count < N:
-        print(leng-1)
-else:
-    print(s_length)
+count = 0
+for cable in Lan_cables:
+    count += cable // top
 
+print(top if count >= N else bottom)
+
+#%%
+# 참조 답
+# 깔끔, 쉬움
+
+from sys import stdin
+K, N = map(int,stdin.readline().split())
+li = list(map(int,stdin.readlines()))
+h, l = sum(li)//N, 1
+while l <= h :
+    mid = (h+l)//2
+    cnt = sum([x//mid for x in li])
+    if cnt < N:
+        h = mid - 1
+    elif cnt >= N:
+        l = mid + 1
+        ans = mid
+print(ans)
---
+++ Coding Test/BAEKJOON/CLASS 2/1874_스택 수열.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 30 20:50:05 2021
+
+@author: 이충섭
+"""
+
+# 1874th, 스택 수열 | class 2
+
+import sys
+
+n = int(next(sys.stdin))
+
+output = []
+stack = []
+last_num = 0
+
+for num_str in sys.stdin:
+    num = int(num_str)
+    
+    if num > last_num:
+        for x in range(last_num + 1, num + 1):
+            output.append('+')
+            stack.append(x)
+            
+        last_num = num
+        
+    elif num != stack[-1]:
+        output = ['NO']
+        break
+    
+    output.append('-')
+    stack.pop()
+
+sys.stdout.write('\n'.join(output))
+
+#%%
+# 참조 답
+# 알고리즘은 같지만 더 편한 사용법
+
+import sys
+
+input = sys.stdin.read
+
+
+def sol1874():
+    n, *nums = map(int, input().split())
+    cur = 1
+    st = []
+    answer = []
+    for num in nums:
+        while cur <= num:
+            st.append(cur)
+            answer.append('+')
+            cur += 1
+        if st[-1] != num:
+            answer = ['NO']
+            break
+        st.pop()
+        answer.append('-')
+    print('\n'.join(answer))
+   
+
+if __name__ == '__main__':
+    sol1874()
