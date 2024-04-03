6951463a09b83740cf6afc398d9d9961893ae2fb
csLEE <39114516+sogNok@users.noreply.github.com>
2024-04-03 11:07:21 +0900

    add day1 daily coding test 3

---
+++ Coding Test/BAEKJOON/CLASS 1/25083_새싹.py
@@ -0,0 +1,17 @@
+# 25083rd, 새싹 | class 1
+import time
+
+start = time.time()
+# print('time', time.time() - start)
+
+import sys
+
+sread = sys.stdin.readline
+swrite = sys.stdout.write
+
+swrite(' '*9 + ',r\'\"7' + '\n')
+swrite('r`-_   ,\'  ,/' + '\n')
+swrite(' \\. \". L_r\'' + '\n')
+swrite('   `~\\/' + '\n')
+swrite(' '*6 + '|' + '\n')
+swrite(' '*6 + '|' + '\n')
